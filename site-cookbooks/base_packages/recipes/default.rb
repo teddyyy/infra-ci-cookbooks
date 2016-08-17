e83d9ea17c41e48cde60c52a9342f1054a48bfd4
teddy <teddy@sfc.wide.ad.jp>
2016-08-17 20:35:53 +0900

    Initial commit

---
+++ site-cookbooks/base_packages/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: base_packages
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+%w{
+	dstat
+	git
+	tmux
+	vim-enhanced
+	yum-plugin-versionlock
+	zsh
+}.each do |pkg|
+	package pkg do
+		action :install
+	end
+end
